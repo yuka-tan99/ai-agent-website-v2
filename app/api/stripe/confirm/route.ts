13ed681c74fbe6fc7402ddb5ac1cfc2892f190d4
Yuchen Tan <yuka.tan@outlook.com>
2025-09-19 13:00:35 -0400

    fix vercel error

--- app/api/stripe/confirm/route.ts
+++ app/api/stripe/confirm/route.ts
@@ -1,4 +1,6 @@
 // app/api/stripe/confirm/route.ts
+export const dynamic = 'force-dynamic'
+export const runtime = 'nodejs'
 import { NextRequest, NextResponse } from 'next/server'
 import Stripe from 'stripe'
 import { supabaseAdmin } from '@/lib/supabaseServer'
